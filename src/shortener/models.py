cd09899e184aace107fbff7d20718c3468a2f36e
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-09-02 15:07:43 +0300

    Created the URL model.

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+"""
+Run the following two commands if you ever change something in this models file.
+
+python manage.py makemigrations
+python manage.py migrate
+
+
+If you forgot, and stuff starts to break, delete the sqlite databse and run the command below along with the two above
+
+python manage.py createsuperuser
+"""
+
+class KirrURL(models.Model):
+    url = models.CharField(max_length=220, )
+    shortcode = models.CharField(max_length=20, null=False, blank=False, default='testval', unique=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
+        return str(self.url)
